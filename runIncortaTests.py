9fb126c4327c2750217a6fc2397fd209850c3eb2
Anahit Sarao <anahit.sarao@incorta.com>
2016-08-01 11:49:15 -0700

    Uncommented Json Validation

--- runIncortaTests.py
+++ runIncortaTests.py
@@ -425,7 +425,7 @@ for sub_dir in test_suite_directories:
 
                 if Debug == False:
                     print "\nFinished JSON DASH EXPORT"
-                # Auto_Module.json_validation.validation(test_case_path, test_case_path_wd, output_wd_path, current_test_suite, admin_wd_path)
+                Auto_Module.json_validation.validation(test_case_path, test_case_path_wd, output_wd_path, current_test_suite, admin_wd_path)
 
                 # LOAD VALIDATION
 
