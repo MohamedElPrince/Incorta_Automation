d483a9f5ee473269899dcf108db1547a2f48744f
Anahit Sarao <anahit.sarao@incorta.com>
2016-08-17 20:36:03 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='qa',
+    version='1.0<Sophia>',
+    packages=['config', 'config.settings', 'Auto_Module'],
+    url='http://www.incorta.com',
+    license='Licensed Material - Property of Incorta',
+    author='Anahit Sarao',
+    author_email='anahit.sarao@incorta.com',
+    description='Automation Framework by Incorta Analytics'
+)
