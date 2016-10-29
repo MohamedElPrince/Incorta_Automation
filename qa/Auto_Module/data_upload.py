c3febcbda27652e5535f78effd463d2b7ace5fd1
Anahit Sarao <anahit.sarao@incorta.com>
2016-10-28 18:52:42 -0700

    added extra layer of checking for schemas listed inside shcema.txt

--- qa/Auto_Module/data_upload.py
+++ qa/Auto_Module/data_upload.py
@@ -31,17 +31,15 @@ def load_data(incorta, session, test_case_path, loader_Validation_Path):
     """
 
     fileFound = False
-    for files in os.listdir(test_case_path):
-        if files == 'schema.txt':
-            try:
-                with open(os.path.join(test_case_path, files)) as text_file:
-                    schema_file_list = [line.rstrip('\n') for line in text_file]
-                fileFound = True
-            except Exception, e:
-                print ("schema.txt Not Found Inside Test Case")
-                writeLogMessage('schema.txt Not Found Inside Test Case %s ' % (test_case_path), mainLogger,
-                                'critical')
-
+    schema_file_list = schema_file(test_case_path)
+    for items in schema_file_list:
+        if items != None:
+            fileFound = True
+        else:
+            fileFound = False
+            print ("Invalid Entry inside schema.txt %s" % test_case_path)
+            writeLogMessage('Invalid Entry inside schema.txt %s ' % (test_case_path), mainLogger,
+                            'critical')
     if fileFound == True:
         schema_names = get_load_status(incorta, session, schema_file_list, command='pre_check')
         for key, value in schema_names.iteritems():
@@ -96,6 +94,20 @@ def load_data(incorta, session, test_case_path, loader_Validation_Path):
                 f.close()
 
 
+def schema_file(test_case_path):
+    """
+    """
+    for files in os.listdir(test_case_path):
+        if files == 'schema.txt':
+            try:
+                with open(os.path.join(test_case_path, files)) as text_file:
+                    schema_file_list = [line.rstrip('\n') for line in text_file]
+                    return schema_file_list
+            except Exception, e:
+                print ("schema.txt Not Found Inside Test Case")
+                writeLogMessage('schema.txt Not Found Inside Test Case %s ' % (test_case_path), mainLogger,
+                                'critical')
+
 def get_load_status(incorta, session, schema_file_list, schema_name=None, command='status'):
     """
     Function returns either status of single schema or returns a dictionary of common schemas between the passed in list
@@ -134,73 +146,74 @@ def get_load_status(incorta, session, schema_file_list, schema_name=None, comman
             writeLogMessage("The GET request can not find: %s, check Schema.txt file" % schema_name, mainLogger,
                             'error')
 
-
-def load_validator(incorta_home, export_schema_names_list, full_schema_export_list):
-    COUNT = 0
-    LOAD_COUNTER = 0
-    loaded = False
-    while loaded == False and COUNT < 60:
-        time.sleep(5)
-        schema_list = []
-        int_size = len(full_schema_export_list)
-        size = str(int_size)
-        size = '-' + size
-        cat_string = "\"" + incorta_home + os.sep + 'server' + os.sep + 'logs' + os.sep + 'catalina.out' + "\""
-        cmd = 'cat ' + cat_string + ' | tr -d \'\\000\' | ' + 'grep "\*\*\* Load" ' + """ | tail """ + size
-        p = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-        output = p.split('\n')
-
-        # Waits for Schema to Load
-        if full_schema_export_list[-1] not in output[int_size-1]:
-            print "Still waiting for schema to load..", full_schema_export_list[-1]
-            writeLogMessage('%s %s' % ("Still waiting for schema to load..", full_schema_export_list[-1]), mainLogger, 'info')
-            LOAD_COUNTER += 1
-            if len(schema_list) == int_size:
-                loaded = True
-        # If schema is loaded, the schema name will be appended to schema_list
-        else:
-            for item in output:
-                string = item.split()
-                for index in range(len(string)):
-                    if string[index] == 'completed!':
-                        schema_list.append(string[index-1])
-
-            if len(schema_list) == int_size:
-                loaded = True
-        COUNT += 1
-        print "Loading schema ", full_schema_export_list[-1], COUNT*5, "seconds.."
-        writeLogMessage('%s %s' % (COUNT*5, "seconds.."), mainLogger, 'info')
-    # Loaded schemas are stored in the variable schema_list
-    return schema_list
-
-def schema_load_validatior(export_schema_names_list, full_schema_export_list, Loader_Validation_Path):
-    """
-    Determines The full list of successfully loaded schemas, dif and suc files are generated in the loaded_validator function
-    """
-    if set(export_schema_names_list) == set(full_schema_export_list):
-        print "LOADED DATA VALIDATED\n"
-        writeLogMessage("LOADED DATA VALIDATED\n", mainLogger, 'info')
-        print "Successfully Loaded Schemas: ", export_schema_names_list
-        writeLogMessage('%s %s' % ("Successfully Loaded Schemas: ", export_schema_names_list), mainLogger, 'info')
-
-    else:
-        print "ERROR IN LOADING DATA"
-        writeLogMessage('ERROR IN LOADING DATA', mainLogger, 'warning')
-        diff_set = set(full_schema_export_list) - set(export_schema_names_list)
-        diff_list = list(diff_set)
-        print diff_list
-        writeLogMessage(diff_list, mainLogger, 'warning')
-
-
-def loaded_validator(loaded_schema_names, loaded_schema, Loader_Validation_Path):
-    for schema in loaded_schema:
-        if schema in loaded_schema_names:
-            print "LOAD SUCCESSFUL: ", loaded_schema, "\n"
-            log_name = Loader_Validation_Path + os.sep + str(schema) + '.suc'
-            f = open(log_name, 'w')
-            f.close()
-        else:
-            print "SCHEMA ", schema, " FAILED TO LOAD \n"
-            log_name = Loader_Validation_Path + os.sep + str(schema) + '.dif'
-            f = open(log_name, 'w')
-            f.close()
+#   todo REMMOVE USELESS CODE 10/28/16 - next master merge
+# def load_validator(incorta_home, export_schema_names_list, full_schema_export_list):
+#     COUNT = 0
+#     LOAD_COUNTER = 0
+#     loaded = False
+#     while loaded == False and COUNT < 60:
+#         time.sleep(5)
+#         schema_list = []
+#         int_size = len(full_schema_export_list)
+#         size = str(int_size)
+#         size = '-' + size
+#         cat_string = "\"" + incorta_home + os.sep + 'server' + os.sep + 'logs' + os.sep + 'catalina.out' + "\""
+#         cmd = 'cat ' + cat_string + ' | tr -d \'\\000\' | ' + 'grep "\*\*\* Load" ' + """ | tail """ + size
+#         p = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+#         output = p.split('\n')
+#
+#         # Waits for Schema to Load
+#         if full_schema_export_list[-1] not in output[int_size-1]:
+#             print "Still waiting for schema to load..", full_schema_export_list[-1]
+#             writeLogMessage('%s %s' % ("Still waiting for schema to load..", full_schema_export_list[-1]), mainLogger, 'info')
+#             LOAD_COUNTER += 1
+#             if len(schema_list) == int_size:
+#                 loaded = True
+#         # If schema is loaded, the schema name will be appended to schema_list
+#         else:
+#             for item in output:
+#                 string = item.split()
+#                 for index in range(len(string)):
+#                     if string[index] == 'completed!':
+#                         schema_list.append(string[index-1])
+#
+#             if len(schema_list) == int_size:
+#                 loaded = True
+#         COUNT += 1
+#         print "Loading schema ", full_schema_export_list[-1], COUNT*5, "seconds.."
+#         writeLogMessage('%s %s' % (COUNT*5, "seconds.."), mainLogger, 'info')
+#     # Loaded schemas are stored in the variable schema_list
+#     return schema_list
+#
+# def schema_load_validatior(export_schema_names_list, full_schema_export_list, Loader_Validation_Path):
+#     """
+#     Determines The full list of successfully loaded schemas, dif and suc files are generated in the loaded_validator function
+#     """
+#     if set(export_schema_names_list) == set(full_schema_export_list):
+#         print "LOADED DATA VALIDATED\n"
+#         writeLogMessage("LOADED DATA VALIDATED\n", mainLogger, 'info')
+#         print "Successfully Loaded Schemas: ", export_schema_names_list
+#         writeLogMessage('%s %s' % ("Successfully Loaded Schemas: ", export_schema_names_list), mainLogger, 'info')
+#
+#     else:
+#         print "ERROR IN LOADING DATA"
+#         writeLogMessage('ERROR IN LOADING DATA', mainLogger, 'warning')
+#         diff_set = set(full_schema_export_list) - set(export_schema_names_list)
+#         diff_list = list(diff_set)
+#         print diff_list
+#         writeLogMessage(diff_list, mainLogger, 'warning')
+#
+#
+# def loaded_validator(loaded_schema_names, loaded_schema, Loader_Validation_Path):
+#     for schema in loaded_schema:
+#         if schema in loaded_schema_names:
+#             print "LOAD SUCCESSFUL: ", loaded_schema, "\n"
+#             log_name = Loader_Validation_Path + os.sep + str(schema) + '.suc'
+#             f = open(log_name, 'w')
+#             f.close()
+#         else:
+#             print "SCHEMA ", schema, " FAILED TO LOAD \n"
+#             log_name = Loader_Validation_Path + os.sep + str(schema) + '.dif'
+#             f = open(log_name, 'w')
+#             f.close()
+#   todo REMMOVE USELESS CODE 10/28/16 - next master merge
