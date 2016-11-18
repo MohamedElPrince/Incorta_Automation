b38d3a5b532c67c7b6afc4282eb0243f25060104
Anahit Sarao <anahit.sarao@incorta.com>
2016-11-17 21:41:04 -0800

    finished summary output and detailed output

--- qa/Auto_Module/summary.py
+++ qa/Auto_Module/summary.py
@@ -5,19 +5,27 @@ import sys
 def print_table(*time_list, **test_suite_name_dict):
     """ """
     detail_table = PrettyTable()
-    header_table = PrettyTable()
-    header_table.field_names= ['Detailed Summary','-','--']
-    header_table.add_row(['Runtime',str(time_list[0])+' seconds',str(time_list[1])+' Minutes'])
-    print header_table
-    detail_table.field_names = ['Test Suites', 'Test Cases', '# of Suc Files', '# of Diff Files', 'Pass Rate']
+    detail_header_table = PrettyTable()
+    summary_header_table = PrettyTable()
+    summary_table = PrettyTable()
+    detail_header_table.field_names= ['Detailed Summary','-','--']
+    detail_header_table.add_row(['Runtime',str(time_list[0])+' seconds',str(time_list[1])+' Minutes'])
+    detail_table.field_names = ['Test Suites', 'Test Cases', '# of Suc Files', '# of Diff Files', 'Total File Count']
+
+    summary_header_table.field_names = ['Overall Summary']
+    summary_table.field_names = ['Test Suites', 'Pass Rate', 'Status']
+    overall_total = 0
+    overall_exception_files = 0
+    overall_suc_count = 0
+    overall_diff_count = 0
+    pass_rate = 0
+    status = 'Success'
     for suite_name, values in test_suite_name_dict.iteritems():
         for case_name, files in values.iteritems():
             total = 0
             exception_files = 0
-            all_file_count = 0
             suc_count = 0
             diff_count= 0
-            pass_rate = 0
             for file in files:
                 total += 1
                 if file.endswith('.suc'):
@@ -27,28 +35,27 @@ def print_table(*time_list, **test_suite_name_dict):
                 else:
                     print 'Unknown file found inside Directory -- Invoke Pass Exception'
                     exception_files =+ 1
+            overall_total += total
+            overall_exception_files += exception_files
+            overall_suc_count += suc_count
+            overall_diff_count += diff_count
+            detail_table.add_row([suite_name, case_name, suc_count, diff_count, total])
+        try:
+            pass_rate = (float((overall_suc_count + overall_diff_count + overall_exception_files) / overall_total)) * 100
+            if pass_rate < 100:
+                status = 'Failed'
+            summary_table.add_row([suite_name, pass_rate, status])
+        except ZeroDivisionError as zerrno:
+            print "\nI/O error({0})".format(zerrno.args)
+            print '\nZero Division Exception, No Ouput Files found or No tests ran'
+            print '\nDetailed Summary Shutting Down'
+        except:
+            print "Unexpected error:", sys.exc_info()
+            raise
 
-            try:
-                pass_rate = (float((suc_count + diff_count + exception_files)/total)) * 100
-            except ZeroDivisionError as zerrno:
-                print "\nI/O error({0})".format(zerrno.args)
-                print '\nZero Division Exception, No Ouput Files found or No tests ran'
-                print '\nDetailed Summary Shutting Down'
-            except:
-                print "Unexpected error:", sys.exc_info()
-                raise
-
-            detail_table.add_row([suite_name, case_name, suc_count, diff_count, pass_rate])
-
+    print '\n'
+    print detail_header_table
     print detail_table
-
-# for suite, case in test_suite_name_dict.iteritems():
-#     if suite != None:
-#         name_list.append(suite)
-#     else:
-#         pass
-# test_suite_name_list = [(str(x), str((x))) for x in test_suite_name_dict]
-# col_width = [max(len(x) for x in col) for col in zip(*test_suite_name_list)]
-# for line in test_suite_name_list:
-#     print "| " + " | ".join("{:{}}".format(x, col_width[i])
-#                             for i, x in enumerate(line)) + " |"
+    print '\n'
+    print summary_header_table
+    print summary_table
