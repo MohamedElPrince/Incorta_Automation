6345f25e53dfeef4cb9e3b3804457b32dbed4edc
Anahit Sarao <anahit.sarao@incorta.com>
2017-10-25 21:50:46 -0700

    release code done

--- logparser/incortalog.py
+++ logparser/incortalog.py
@@ -3,20 +3,23 @@ import os
 from errno import ENOENT
 import csv
 
-ROOT_DIR = os.path.dirname(__file__) # This is your Project Root
+ROOT_DIR = os.path.dirname(__file__)  # This is your Project Root
+
 
 def check_logfile(file):
     """
+    Check for logfile, at given path.
     """
     if not os.path.isfile(file):
         raise IOError(ENOENT, 'Not a file', file)
+
 
 def parse_logfile(file, schema_name):
     """
     """
     with open(file) as fp2:
         content = fp2.read()
-    print("There are {}, Schemas Loaded in {}").format(content.count("Finished"),file) # Extra detail -- Fun
+    print("There are {}, Schemas Loaded in {}").format(content.count("Finished"), file)  # Extra detail -- Fun
 
     data = []
     parsed_data = []
@@ -29,22 +32,14 @@ def parse_logfile(file, schema_name):
         if "Finished" in i:
             if "INFO" in i:
                 if (schema_name + "]") in i:
-                    parsed_data.append(i.split("]"))
+                    parsed_data.append(i.split(","))
 
     filtered_data = []
     for i in parsed_data:
         for i2 in i:
-            filtered_data.append(i2.split("["))
-    for item in filtered_data:
-        if "" in item:
-            filtered_data.remove(item)
-        for items in item:
-            if items.isspace():
-                item.remove(items)
-    csv_list = []
-    for i in filtered_data:
-        print i
+            filtered_data.append(i2.split(" "))
     csv_writer(filtered_data)
+
 
 def csv_writer(data, path="output.csv"):
     """
@@ -55,19 +50,21 @@ def csv_writer(data, path="output.csv"):
         for line in data:
             writer.writerow(line)
 
-
-
 def main():
     """
+    Main function handles arguments
     """
     print("Sample Input: -f /home/incorta/Incorta.2017-09-24.log -s jira-incremental")
-    parser = argparse.ArgumentParser(description='Description for Custom LogParser', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-f', '--file', help='Logfile to be Parsed. \nSample Input: /home/incorta/incorta.2017.log', required=True)
-    parser.add_argument('-s', '--schema', help='Schema name. \nSample Input: jira-incremental' , required=True)
+    parser = argparse.ArgumentParser(description='Description for Custom LogParser',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-f', '--file', help='Logfile to be Parsed. \nSample Input: /home/incorta/incorta.2017.log',
+                        required=True)
+    parser.add_argument('-s', '--schema', help='Schema name. \nSample Input: jira-incremental', required=True)
     args = parser.parse_args()
     print('Current Arguments: {}').format(args)
     check_logfile(args.file)
     parse_logfile(args.file, args.schema)
+
 
 if __name__ == '__main__':
     """Main"""
