2d3bb2b329c0c9a460d09f28a5d64a43516e5ef7
Mohab Mohie <Mohab.Mohie@incorta.com>
2018-05-13 13:59:31 +0200

    updating drivers, prepping users, and attempting upload fix for firefox

--- src/test/java/tests/gui/security/Users.java
+++ src/test/java/tests/gui/security/Users.java
@@ -1,5 +1,19 @@
 package tests.gui.security;
 
-public class Users {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
+import com.shaftEngine.ioActionLibrary.ExcelFileManager;
+
+import io.qameta.allure.Epic;
+
+@Epic("incorta > Security > Users.")
+public class Users {
+	// Declaring web-driver and excel reader instances
+	WebDriver driver;
+	ExcelFileManager testDataReader;
+
+  @Test
+  public void f() {
+  }
 }
