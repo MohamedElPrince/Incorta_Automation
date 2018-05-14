68b96de72cdc943d94756d8ebafbc4ebca3fe272
abdelsalam <abdelsalam@172.20.16.121>
2018-05-14 12:16:38 +0200

    Script Updated with removing unneeded comments and variables.

--- src/test/java/tests/gui/security/Groups.java
+++ src/test/java/tests/gui/security/Groups.java
@@ -17,7 +17,6 @@ import io.qameta.allure.SeverityLevel;
 import pageObjectModels.modules.login.Login_Login;
 import pageObjectModels.modules.main.Main_Skeleton;
 import pageObjectModels.modules.security.Security_Groups;
-import pageObjectModels.modules.security.Security_Users;
 
 
 @Epic("Incorta -> Security -> Groups")
@@ -29,28 +28,22 @@ import pageObjectModels.modules.security.Security_Users;
 		
 		// Declaring Page Objects that will be used in the tests
 		Security_Groups groupsPage;
-		Security_Users usersPage;
 		Login_Login loginPage;
-		Main_Skeleton pageHeader;
+		Main_Skeleton mainPage;
 		
 		
 		//Declaring Variables that will be used in below tests
 		String name;
-		String tenant;
-		String username;
-		String password;
 
-		
-		@Test(priority = 2, description = "TC_C474 - Create New Group.")
+		@Test(priority = 1, description = "TC_C474 - Create New Group.")
 		@Description("Given I've logged in. When I navigate to Security Tab, And go to Groups and click on the "+" and add Group name and description, Click 'Add User' Button. Then, A new group will be added to group list.")
 		@Severity(SeverityLevel.CRITICAL)
 		public void createNewGroup() 
 		{
 			groupsPage = new Security_Groups(driver);
-			pageHeader = new Main_Skeleton(driver);
+			mainPage = new Main_Skeleton(driver);
 			groupsPage.Navigate_toURL();
-			pageHeader.Click_add();
-			//groupsPage.AddNewGroup(name, description);
+			mainPage.Click_add();
 			name = groupsPage.AddNewGroup();
 			groupsPage.Navigate_toURL();
 			groupsPage.Assert_groupIsDisplayed(name);
