843f2fbfa129936b9cc6b13a78214228b03eb4ea
Mohab Mohie <Mohab.Mohie@incorta.com>
2018-08-07 17:28:19 +0200

    fix 3

---
+++ src/test/java/pageObjectModels/login/NewUI_SignOut.java
@@ -0,0 +1,37 @@
+package pageObjectModels.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.shaftEngine.elementActionLibrary.ElementActions;
+import com.shaftEngine.ioActionLibrary.ExcelFileManager;
+import com.shaftEngine.validationsLibrary.Assertions;
+
+public class NewUI_SignOut {
+	//// Variables
+	WebDriver driver;
+	ExcelFileManager testDataReader = new ExcelFileManager(System.getProperty("testDataFilePath"));
+	String signedOutMessageHeader = testDataReader.getCellData("signedOutMessageHeader");
+	String signedOutMessageBody = testDataReader.getCellData("signedOutMessageBody");
+
+	//// Elements
+	By body_signedOutMessageHeader_label = By
+			.xpath("//div[@class='inc-login__forget-password-conf-container-reset-conf-text']/span");
+	By body_signedOutMessageBody_label = By
+			.xpath("//div[@class='inc-login__forget-password-conf-container-reset-conf-text-body']/span");
+	By body_goToSignInPage_link = By.xpath("//a[@onclick='login()']");
+
+	//// Functions
+	public NewUI_SignOut(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void assert_signOutMessageHeaderAndBodyAreCorrect() {
+		Assertions.assertElementAttribute(driver, body_signedOutMessageHeader_label, "text", signedOutMessageHeader,
+				true);
+		Assertions.assertElementAttribute(driver, body_signedOutMessageBody_label, "text", signedOutMessageBody, true);
+	}
+
+	public void navigate_toLoginPage() {
+		ElementActions.click(driver, body_goToSignInPage_link);
+	}
+}
