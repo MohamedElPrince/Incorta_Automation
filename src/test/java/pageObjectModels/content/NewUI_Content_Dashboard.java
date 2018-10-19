a4b4f22dca511d2d9d471e96721357c5472c3517
Mohab Mohie <Mohab.Mohie@incorta.com>
2018-10-19 15:05:54 +0200

    minor modifications to logging in the dashboard crawler

--- src/test/java/pageObjectModels/content/NewUI_Content_Dashboard.java
+++ src/test/java/pageObjectModels/content/NewUI_Content_Dashboard.java
@@ -10,222 +10,225 @@ import com.shaft.io.ReportManager;
 import com.shaft.validation.Assertions;
 
 public class NewUI_Content_Dashboard {
-    //// Variables
-    WebDriver driver;
-    ExcelFileManager testDataReader = new ExcelFileManager(System.getProperty("testDataFilePath"));
-    int customElementIdentificationTimeout = 1;
-    int customNumberOfRetries = 1;
+	//// Variables
+	WebDriver driver;
+	ExcelFileManager testDataReader = new ExcelFileManager(System.getProperty("testDataFilePath"));
+	int customElementIdentificationTimeout = 1;
+	int customNumberOfRetries = 1;
 
-    //// Elements
-    // first nested header
-    By pageDetails_dashboardName_label = By.xpath("//div[@class='page-details--headerwrap']//h1");
-    By pageDetails_back_button = By.xpath("//a[contains(@class,'page-details-back-button')]");
-    By pageDetails_details_link = By.xpath("//button[contains(@class,'page-details--link')]");
-    By pageDetails_setAsDefaultDashboard_button = By
-	    .xpath("//i[contains(@class,'anticon-pushpin-o')]/ancestor::button");
-    By pageDetails_dashboardActionMenu_button = By
-	    .xpath("//div[@class='inc-dashboard__actions']//button[contains(@class,'dashboard-action-menu')]");
-    By pageDetails_add_button = By
-	    .xpath("//span[@class='inc-toolbar-button']//*[@data-icon='plus']/ancestor::a[contains(@href,'analyze')]");
+	//// Elements
+	// first nested header
+	By pageDetails_dashboardName_label = By.xpath("//div[@class='page-details--headerwrap']//h1");
+	By pageDetails_back_button = By.xpath("//a[contains(@class,'page-details-back-button')]");
+	By pageDetails_details_link = By.xpath("//button[contains(@class,'page-details--link')]");
+	By pageDetails_setAsDefaultDashboard_button = By
+			.xpath("//i[contains(@class,'anticon-pushpin-o')]/ancestor::button");
+	By pageDetails_dashboardActionMenu_button = By
+			.xpath("//div[@class='inc-dashboard__actions']//button[contains(@class,'dashboard-action-menu')]");
+	By pageDetails_add_button = By
+			.xpath("//span[@class='inc-toolbar-button']//*[@data-icon='plus']/ancestor::a[contains(@href,'analyze')]");
 
-    // second nested header
-    By filterBar_bookmark_button = By.xpath("//div[@class='inc-filter-bar']//button[contains(@class,'bm-icon')]");
-    By filterBar_filter_button = By.xpath("//div[@class='inc-filter-bar']//button[contains(@class,'filter-icon')]");
-    By filterBar_search_textBox = By.xpath("//div[@class='inc-filter-bar']//input");
+	// second nested header
+	By filterBar_bookmark_button = By.xpath("//div[@class='inc-filter-bar']//button[contains(@class,'bm-icon')]");
+	By filterBar_filter_button = By.xpath("//div[@class='inc-filter-bar']//button[contains(@class,'filter-icon')]");
+	By filterBar_search_textBox = By.xpath("//div[@class='inc-filter-bar']//input");
 
-    // spinner
-    By loadingSpinner = By.xpath("//span[contains(@class,'ant-spin-dot')]");
-    By genericInsight_div = By.xpath(
-	    "//div[@class='insight__body']/ancestor::div[@class='draggable-insight-wrapper']|//div[@class='error-loading-wrapper__error']/ancestor::div[@class='draggable-insight-wrapper']");
-    By insight_div;
+	// spinner
+	By loadingSpinner = By.xpath("//span[contains(@class,'ant-spin-dot')]");
+	By genericInsight_div = By.xpath(
+			"//div[@class='insight__body']/ancestor::div[@class='draggable-insight-wrapper']|//div[@class='error-loading-wrapper__error']/ancestor::div[@class='draggable-insight-wrapper']");
+	By insight_div;
 
-    // body-insightHeader
-    By insightHeader_insightName_label = By.className("insight__header-title");
-    By insightHeader_insightActions_button = By.xpath("//span[@class='insight__header-actions']/button");
+	// body-insightHeader
+	By insightHeader_insightName_label = By.className("insight__header-title");
+	By insightHeader_insightActions_button = By.xpath("//span[@class='insight__header-actions']/button");
 
-    // body-insightBody-aggregatedTable
-    By aggregatedTable_columnHeader_link;
-    By aggregatedTable_cellValue_link;
+	// body-insightBody-aggregatedTable
+	By aggregatedTable_columnHeader_link;
+	By aggregatedTable_cellValue_link;
 
-    // popup settings dashboard menu
-    By popup_dashboard_folder_shareAccess_button = By.xpath("//a/span[contains(text(),'Share Access')]");
-    By popup_dashboard_folder_sendNow_button = By.xpath("//a/span[contains(text(),'Send Now')]");
-    By popup_dashboard_folder_scheduleDelivery_button = By.xpath("//a/span[contains(text(),'Schedule Delivery')]");
+	// popup settings dashboard menu
+	By popup_dashboard_folder_shareAccess_button = By.xpath("//a/span[contains(text(),'Share Access')]");
+	By popup_dashboard_folder_sendNow_button = By.xpath("//a/span[contains(text(),'Send Now')]");
+	By popup_dashboard_folder_scheduleDelivery_button = By.xpath("//a/span[contains(text(),'Schedule Delivery')]");
 
-    // body Dashboard
-    By body_shareButton = By.xpath("//i[@class='anticon anticon-share-alt']");
-    // set_default Dashboard pin
-    By body_unpushed_pin = By.xpath(
-	    "//button[contains(@class, 'inc-clickable') and contains(@class ,'page-details-default')] | //svg[@data-icon='pushpin']");
-    By body_pushed_pin = By.xpath(
-	    "//button[contains(@class, 'inc-clickable') and contains(@class ,'isDefault')] | //svg[@data-icon='pushpin']");
+	// body Dashboard
+	By body_shareButton = By.xpath("//i[@class='anticon anticon-share-alt']");
+	// set_default Dashboard pin
+	By body_unpushed_pin = By.xpath(
+			"//button[contains(@class, 'inc-clickable') and contains(@class ,'page-details-default')] | //svg[@data-icon='pushpin']");
+	By body_pushed_pin = By.xpath(
+			"//button[contains(@class, 'inc-clickable') and contains(@class ,'isDefault')] | //svg[@data-icon='pushpin']");
 
-    By body_bookmark = By.xpath("//i[@class='anticon anticon-book']");
-    By popup_bookmarksName;
+	By body_bookmark = By.xpath("//i[@class='anticon anticon-book']");
+	By popup_bookmarksName;
 
-    By body_filter = By.xpath("//i[@class='anticon anticon-filter']");
-    By popup_filtersName;
+	By body_filter = By.xpath("//i[@class='anticon anticon-filter']");
+	By popup_filtersName;
 
-    //// Functions
-    public NewUI_Content_Dashboard(WebDriver driver) {
-	this.driver = driver;
-	waitForDashboardToFullyLoad();
-    }
-
-    public void reportcurrentDashboardURL() {
-	ReportManager.log("Dashboard URL: [" + BrowserActions.getCurrentURL(driver) + "].");
-    }
-
-    public void waitForDashboardToFullyLoad() {
-	int loadingSpinners = ElementActions.getElementsCount(driver, loadingSpinner,
-		customElementIdentificationTimeout, customNumberOfRetries);
-	while (loadingSpinners > 0) {
-	    loadingSpinners = ElementActions.getElementsCount(driver, loadingSpinner,
-		    customElementIdentificationTimeout, customNumberOfRetries);
-	}
-    }
-
-    public int countInsights() {
-	return ElementActions.getElementsCount(driver, genericInsight_div, customElementIdentificationTimeout,
-		customNumberOfRetries);
-    }
-
-    public void assert_insightContent_isDisplayed(int insightIndex) {
-	insight_div = By.xpath(
-		"(//div[@class='insight__body']/ancestor::div[@class='draggable-insight-wrapper']|//div[@class='error-loading-wrapper__error']/ancestor::div[@class='draggable-insight-wrapper'])["
-			+ insightIndex + "]");
-	Assertions.assertElementExists(driver, insight_div, true);
-    }
-
-    /**
-     * Asserts that the target dashboardName is correctly displayed in the
-     * pageDetails_dashboardName_label
-     * 
-     * @param dashboardName
-     */
-    public void assert_dashboardName_isCorrect(String dashboardName) {
-	// replaceRegex
-	Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label, "text", dashboardName, 1, true);
-	// Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label,
-	// "text", dashboardName, true);
-    }
-
-    /**
-     * Asserts that the displayed dashboardName matches the desired one
-     * 
-     * @param dashboardName
-     */
-    public void assert_dashboardName_matches(String dashboardName) {
-	Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label, "text", dashboardName, true);
-    }
-
-    /**
-     * Asserts that the target insightName is correctly displayed in the
-     * insightName_label
-     * 
-     * @param insightName
-     */
-    public void assert_insightName_isCorrect(String insightName) {
-	Assertions.assertElementAttribute(driver, insightHeader_insightName_label, "text", insightName, true);
-    }
-
-    /**
-     * Given that the insightType is aggregatedTable, Asserts that the actual
-     * columnHeaderValue matches with the expected one
-     * 
-     * @param columnNumber
-     *            the desired column number; 1 is the first column table header
-     * @param columnHeaderValue
-     *            the expected value of the target cell
-     */
-    public void aggregatedTable_assert_columnHeader_isCorrect(int columnNumber, String columnHeaderValue) {
-	aggregatedTable_columnHeader_link = By.xpath("(//thead//span[@class='colHeader']//a)[" + columnNumber + "]");
-	Assertions.assertElementAttribute(driver, aggregatedTable_columnHeader_link, "text", columnHeaderValue, true);
-    }
-
-    /**
-     * Given that the insightType is aggregatedTable, Asserts that the actual
-     * cellValue matches with the expected one
-     * 
-     * @param typeOrValue
-     *            "type" refers to the first column, "value" refers to the second
-     *            column
-     * @param rowNumber
-     *            the desired row number; 1 is the first row after the table header
-     * @param cellValue
-     *            the expected value of the target cell
-     */
-    public void aggregatedTable_assert_cellValue_isCorrect(String typeOrValue, int rowNumber, String cellValue) {
-	switch (typeOrValue.trim().toLowerCase()) {
-	case "type":
-	    aggregatedTable_cellValue_link = By
-		    .xpath("//tbody/tr[" + rowNumber + "]//a[contains(@class,'table__body-cell--aggregated')]");
-	    break;
-	case "value":
-	    aggregatedTable_cellValue_link = By
-		    .xpath("//tbody/tr[" + rowNumber + "]//a[contains(@class,'table__body-cell--basic')]");
-	    break;
-	default:
-	    break;
+	//// Functions
+	public NewUI_Content_Dashboard(WebDriver driver) {
+		this.driver = driver;
+		waitForDashboardToFullyLoad();
 	}
 
-	Assertions.assertElementAttribute(driver, aggregatedTable_cellValue_link, "text", cellValue, true);
-    }
+	public void reportcurrentDashboardURL() {
+		ReportManager.log("Dashboard URL: [" + BrowserActions.getCurrentURL(driver) + "].");
+	}
 
-    public void assert_shared_button_active() {
-	Assertions.assertElementExists(driver, popup_dashboard_folder_shareAccess_button, true);
-    }
+	public void waitForDashboardToFullyLoad() {
+		int loadingSpinners = ElementActions.getElementsCount(driver, loadingSpinner,
+				customElementIdentificationTimeout, customNumberOfRetries);
+		if (loadingSpinners > 0) {
+			ReportManager.log("Waiting for dashboard and all insights to fully load.");
+		}
+		while (loadingSpinners > 0) {
+			loadingSpinners = ElementActions.getElementsCount(driver, loadingSpinner,
+					customElementIdentificationTimeout, customNumberOfRetries);
+		}
+	}
 
-    public void assert_shared_button_disabled() {
-	Assertions.assertElementExists(driver, popup_dashboard_folder_shareAccess_button, false);
-    }
+	public int countInsights() {
+		return ElementActions.getElementsCount(driver, genericInsight_div, customElementIdentificationTimeout,
+				customNumberOfRetries);
+	}
 
-    public void click_shareOptions_sendNow_button() {
-	ElementActions.click(driver, body_shareButton);
-	ElementActions.click(driver, popup_dashboard_folder_sendNow_button);
-    }
+	public void assert_insightContent_isDisplayed(int insightIndex) {
+		insight_div = By.xpath(
+				"(//div[@class='insight__body']/ancestor::div[@class='draggable-insight-wrapper']|//div[@class='error-loading-wrapper__error']/ancestor::div[@class='draggable-insight-wrapper'])["
+						+ insightIndex + "]");
+		Assertions.assertElementExists(driver, insight_div, true);
+	}
 
-    public void click_shareOptions_scheduleDelivery_button() {
-	ElementActions.click(driver, body_shareButton);
-	ElementActions.click(driver, popup_dashboard_folder_scheduleDelivery_button);
-    }
+	/**
+	 * Asserts that the target dashboardName is correctly displayed in the
+	 * pageDetails_dashboardName_label
+	 * 
+	 * @param dashboardName
+	 */
+	public void assert_dashboardName_isCorrect(String dashboardName) {
+		// replaceRegex
+		Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label, "text", dashboardName, 1, true);
+		// Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label,
+		// "text", dashboardName, true);
+	}
 
-    // Set default dashboard pin
+	/**
+	 * Asserts that the displayed dashboardName matches the desired one
+	 * 
+	 * @param dashboardName
+	 */
+	public void assert_dashboardName_matches(String dashboardName) {
+		Assertions.assertElementAttribute(driver, pageDetails_dashboardName_label, "text", dashboardName, true);
+	}
 
-    public void assert_SetDefault_DashboardPin_Displayed() {
+	/**
+	 * Asserts that the target insightName is correctly displayed in the
+	 * insightName_label
+	 * 
+	 * @param insightName
+	 */
+	public void assert_insightName_isCorrect(String insightName) {
+		Assertions.assertElementAttribute(driver, insightHeader_insightName_label, "text", insightName, true);
+	}
 
-	Assertions.assertElementExists(driver, body_unpushed_pin, true);
+	/**
+	 * Given that the insightType is aggregatedTable, Asserts that the actual
+	 * columnHeaderValue matches with the expected one
+	 * 
+	 * @param columnNumber
+	 *            the desired column number; 1 is the first column table header
+	 * @param columnHeaderValue
+	 *            the expected value of the target cell
+	 */
+	public void aggregatedTable_assert_columnHeader_isCorrect(int columnNumber, String columnHeaderValue) {
+		aggregatedTable_columnHeader_link = By.xpath("(//thead//span[@class='colHeader']//a)[" + columnNumber + "]");
+		Assertions.assertElementAttribute(driver, aggregatedTable_columnHeader_link, "text", columnHeaderValue, true);
+	}
 
-    }
+	/**
+	 * Given that the insightType is aggregatedTable, Asserts that the actual
+	 * cellValue matches with the expected one
+	 * 
+	 * @param typeOrValue
+	 *            "type" refers to the first column, "value" refers to the second
+	 *            column
+	 * @param rowNumber
+	 *            the desired row number; 1 is the first row after the table header
+	 * @param cellValue
+	 *            the expected value of the target cell
+	 */
+	public void aggregatedTable_assert_cellValue_isCorrect(String typeOrValue, int rowNumber, String cellValue) {
+		switch (typeOrValue.trim().toLowerCase()) {
+		case "type":
+			aggregatedTable_cellValue_link = By
+					.xpath("//tbody/tr[" + rowNumber + "]//a[contains(@class,'table__body-cell--aggregated')]");
+			break;
+		case "value":
+			aggregatedTable_cellValue_link = By
+					.xpath("//tbody/tr[" + rowNumber + "]//a[contains(@class,'table__body-cell--basic')]");
+			break;
+		default:
+			break;
+		}
 
-    public void click_SetDefault_Dashboardpin() {
+		Assertions.assertElementAttribute(driver, aggregatedTable_cellValue_link, "text", cellValue, true);
+	}
 
-	ElementActions.click(driver, body_unpushed_pin);
-	Assertions.assertElementExists(driver, body_pushed_pin, true);
+	public void assert_shared_button_active() {
+		Assertions.assertElementExists(driver, popup_dashboard_folder_shareAccess_button, true);
+	}
 
-    }
+	public void assert_shared_button_disabled() {
+		Assertions.assertElementExists(driver, popup_dashboard_folder_shareAccess_button, false);
+	}
 
-    public void click_bookmarkButton() {
-	ElementActions.click(driver, body_bookmark);
-    }
+	public void click_shareOptions_sendNow_button() {
+		ElementActions.click(driver, body_shareButton);
+		ElementActions.click(driver, popup_dashboard_folder_sendNow_button);
+	}
 
-    public void assert_bookmarksExist(String BookmarkName) {
-	popup_bookmarksName = By.xpath("//div[@class='inc-bookmarks-list-item'][contains(.,'" + BookmarkName + "')]");
-	Assertions.assertElementExists(driver, popup_bookmarksName, true);
-    }
+	public void click_shareOptions_scheduleDelivery_button() {
+		ElementActions.click(driver, body_shareButton);
+		ElementActions.click(driver, popup_dashboard_folder_scheduleDelivery_button);
+	}
 
-    public void click_filterButton() {
-	ElementActions.click(driver, body_filter);
-    }
+	// Set default dashboard pin
 
-    public void addNewInsight() {
-	ElementActions.click(driver, pageDetails_add_button);
-    }
+	public void assert_SetDefault_DashboardPin_Displayed() {
 
-    public void assert_filterApplied(String FilterName) {
-	popup_filtersName = By
-		.xpath("//span[@class='inc-filter-master__menu-item'][contains(.,'" + FilterName + "')]/span");
-	Assertions.assertElementAttribute(driver, popup_filtersName, "class", "([\\s\\S]*)applied", true);
-    }
+		Assertions.assertElementExists(driver, body_unpushed_pin, true);
+
+	}
+
+	public void click_SetDefault_Dashboardpin() {
+
+		ElementActions.click(driver, body_unpushed_pin);
+		Assertions.assertElementExists(driver, body_pushed_pin, true);
+
+	}
+
+	public void click_bookmarkButton() {
+		ElementActions.click(driver, body_bookmark);
+	}
+
+	public void assert_bookmarksExist(String BookmarkName) {
+		popup_bookmarksName = By.xpath("//div[@class='inc-bookmarks-list-item'][contains(.,'" + BookmarkName + "')]");
+		Assertions.assertElementExists(driver, popup_bookmarksName, true);
+	}
+
+	public void click_filterButton() {
+		ElementActions.click(driver, body_filter);
+	}
+
+	public void addNewInsight() {
+		ElementActions.click(driver, pageDetails_add_button);
+	}
+
+	public void assert_filterApplied(String FilterName) {
+		popup_filtersName = By
+				.xpath("//span[@class='inc-filter-master__menu-item'][contains(.,'" + FilterName + "')]/span");
+		Assertions.assertElementAttribute(driver, popup_filtersName, "class", "([\\s\\S]*)applied", true);
+	}
 
 }
